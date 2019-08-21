89c01acc9e8600d0da698260fc995e9d77093999
Kewin Dousse <me@protectator.ch>
2019-08-22 01:43:02 +0200

    Some cleaning

--- displayPPMimg.py
+++ displayPPMimg.py
@@ -1,14 +1,11 @@
 """
 displayPPMimg
-
 Displays a .PPM image on the card10 badge
-
 The PPM has to have the following format :
 format: binary (magic number P6)
 width: 160px
 height: 80px
 max value: 255
-
 Author: Kewin Dousse (Protectator)
 """
 import color
@@ -16,17 +13,6 @@ import display
 import ledfx
 
 FILENAME = 'data.ppm'
-
-def microTry():
-    try:
-        main()
-    except Exception as e:
-        ledfx.kitt()
-        with display.open() as disp:
-            disp.clear()
-            disp.print(str(e), fg=color.WHITE, bg=color.BLACK, posx=0, posy=0)
-            disp.update()
-            disp.close()
 
 def bytes_to_int(bytes):
     result = 0
@@ -36,7 +22,10 @@ def bytes_to_int(bytes):
 
 def main():
     image_file = open(FILENAME, "rb")
-    image_file.read(14)
+    magic = image_file.read(8)
+    width = image_file.read(2)
+    height = image_file.read(2)
+    max_value = image_file.read(2)
     with display.open() as disp:
         for y in range(80):
             for x in range(160):
@@ -49,21 +38,5 @@ def main():
         disp.close()
     image_file.close()
 
-def sensors():
-    light_sensor.start()
-    n = 0
-    while True:
-        n = n + 1
-        with display.open() as disp:
-            disp.clear()
-            level = light_sensor.get_reading()
-            disp.print(str(level), fg=color.WHITE, bg=color.BLACK, posx=0, posy=0)
-
-            mapped = level * 160 / 400
-            disp.rect(0, 0, int(mapped), 60, col=color.YELLOW)
-
-            disp.update()
-            disp.close()
-
 if __name__ == "__main__":
-    microTry()
+    main()
